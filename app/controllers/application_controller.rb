e57397fa1d78421bbba06ff2a7cd52abeadb101c
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-06-28 08:07:57 +0200

    add authorization resque

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  protected
+  def not_authorized
+    redirect_to root_path, notice: 'Unauthorized'
+  end
 end
