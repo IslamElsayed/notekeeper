1f62324bafef334d881111c3d2bd4570bbe53085
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-06-28 08:11:32 +0200

    updaate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :notes
+  resources :notes do
+    resources :users, only: [:index] do
+      member do
+        delete :revoke
+      end
+      collection do
+        post :invite
+      end
+    end
+  end
+
+  post 'notes/:id', to: 'notes#update'
+  resources :tags, only: [:create]
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'notes#index'
