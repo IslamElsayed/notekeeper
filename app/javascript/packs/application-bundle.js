71b8bf46b0c92f557dae2a805b957af01c2ffa09
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-06-28 08:56:47 +0200

    remove obselete code

--- app/javascript/packs/application-bundle.js
+++ app/javascript/packs/application-bundle.js
@@ -1,13 +1,10 @@
 import ReactOnRails from 'react-on-rails';
-
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 import NotesIndex from '../bundles/components/notes/NotesIndex';
 import NoteForm from '../bundles/components/notes/NoteForm';
 import Note from '../bundles/components/notes/Note';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
   NotesIndex,
   NoteForm,
   Note
