a71a555abacc0a288e8a018dae98e1a7a79d7e6b
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-06-28 08:11:50 +0200

    add notes_count to tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_25_124937) do
+ActiveRecord::Schema.define(version: 2018_06_28_021518) do
 
   create_table "note_users", force: :cascade do |t|
     t.integer "role"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_06_25_124937) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "notes_count"
   end
 
   create_table "users", force: :cascade do |t|
