5d766d256fd302ecfc0a1fce3faba7f478c110e1
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-06-28 08:12:49 +0200

    implement tag notes count

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,29 +4,41 @@ class Note < ApplicationRecord
   # associations
   has_many :note_users, dependent: :destroy
   has_many :users, through: :note_users
-  has_and_belongs_to_many :tags
+  has_and_belongs_to_many :tags, after_add: :increment_tag_notes_count, after_remove: :decrement_tag_notes_count
 
   # callbacks
   after_create :create_note_user_for_creator
+
+  # validations
+  validates :title, presence: true
+  validates :body, presence: true
 
   def note_user_for(user)
     self.note_users.find_by(user: user)
   end
 
-  def share(**args)
+  def share(args)
     self.note_users.create(user_id: args[:user_id], role: args[:role])
   end
 
-  def revoke(user_id)
-    self.note_users.find_by(user_id: user_id).destroy
+  def revoke(user)
+    self.note_users.find_by(user: user).destroy
   end
 
   def creator
-    @creator ||= self.note_users.find_by(role: :creator)
+    @creator ||= self.note_users.find_by(role: :creator).user
   end
 
   private
     def create_note_user_for_creator
       self.note_users.create(user: creator, role: :creator)
+    end
+
+    def increment_tag_notes_count(tag)
+      tag.increment!(:notes_count)
+    end
+
+    def decrement_tag_notes_count(tag)
+      tag.decrement!(:notes_count)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :notes, through: :note_users
 
   # validations
-  validates :name, presence: true, uniqueness: true
+  # validates :name, presence: true, uniqueness: true
 end
