4a81e6ff053dee5e1c73682e30e79929725538bf
Islam Elsayed <islam.elsayed.mohammed@gmail.com>
2018-07-04 17:30:45 +0200

    update writitng style

--- app/policies/note_policy.rb
+++ app/policies/note_policy.rb
@@ -1,21 +1,24 @@
 class NotePolicy < ApplicationPolicy
+  attr_reader :note_user
+
   def initialize(user, record)
     @user = user
     @record = record
+    @note_user = record.note_user_for(user)
   end
 
   def show?
-    record.note_user_for(user) || record.creator == user
+    record
   end
 
   def edit?
-    record.note_user_for(user)&.updater? || record.note_user_for(user)&.owner? || record.creator == user
+    note_user&.updater? || note_user&.owner? || record.creator == user
   end
   alias_method :update?, :edit?
 
   def destroy?
-    record.note_user_for(user).owner? || record.creator == user
+    note_user.owner? || note_user.creator?
   end
-  alias_method :share?, :destroy?
+  alias_method :invite?, :destroy?
   alias_method :revoke?, :destroy?
 end
